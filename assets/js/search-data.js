75fe81cda8cbcb33fe58df37034606feba52958e
christianodenwald <72208292+christianodenwald@users.noreply.github.com>
2026-03-26 12:14:40 +0000

    Deploying to gh-pages from @ christianodenwald/christianodenwald.github.io@9e84f3f19a2e43aa4f645aaf2e5239afe3fa73d3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,16 +39,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-a-dataset-and-codebase-for-the-evaluation-of-theories-of-actual-causation-is-now-available-here",
+          title: 'A dataset and codebase for the evaluation of theories of actual causation is...',
           description: "",
           section: "News",},{id: "projects-actual-causation",
           title: 'actual causation',
